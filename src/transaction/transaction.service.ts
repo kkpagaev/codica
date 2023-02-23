a9c79ec4e862d723236eb37cb835be20f5befc09
kkpagaev <kkpagaev@gmail.com>
2023-02-23 17:35:20 +0200

    transaction actions

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -9,4 +9,30 @@ export class TransactionService {
     @InjectRepository(Transaction)
     private transactionRepository: Repository<Transaction>
   ) {}
+
+  async paginate(page: number, limit: number) {
+    const [transactions, total] = await this.transactionRepository.findAndCount(
+      {
+        skip: (page - 1) * limit,
+        take: limit,
+      }
+    )
+
+    return {
+      transactions,
+      total,
+      page,
+      limit,
+    }
+  }
+
+  async create(transaction: Transaction) {
+    const result = await this.transactionRepository.save(transaction)
+    return result
+  }
+
+  async delete(id: string) {
+    const result = await this.transactionRepository.delete(id)
+    return result
+  }
 }
