3b71a267e7f4c064d9f6c7a756785cc90ec0db97
kkpagaev <kkpagaev@gmail.com>
2023-02-23 14:50:49 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
+}
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+bootstrap()
