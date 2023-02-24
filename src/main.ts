6af4b026758df88b530f7282a436fc58827283eb
kkpagaev <kkpagaev@gmail.com>
2023-02-24 14:05:30 +0200

    static swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+// import { writeFileSync } from "fs"
+// import * as path from "path"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
@@ -16,6 +18,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup("swagger", app, document)
+  // const outputPath = path.resolve(process.cwd(), "swagger.json")
+  // writeFileSync(outputPath, JSON.stringify(document), { encoding: "utf8" })
   await app.listen(3000)
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
