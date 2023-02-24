f93263a47ce9ade97cf617bc3ae5f84db89f1ede
kkpagaev <kkpagaev@gmail.com>
2023-02-24 07:46:34 +0200

    validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 import { AppModule } from "./app.module"
@@ -12,6 +13,7 @@ async function bootstrap() {
     .addTag("transaction")
     .addTag("category")
     .build()
+  app.useGlobalPipes(new ValidationPipe())
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup("swagger", app, document)
   await app.listen(3000)
